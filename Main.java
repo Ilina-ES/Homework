9c87c90f62a990f3af819b0aa69eec6b320249e4
Екатерина <ekaterina@MacBook-Pro-Ekaterina.local>
2023-01-27 08:25:03 +0800

    Выполнено ДЗ № 2 Принципы ООП Абстракция и интерфейсы JAVA

--- Main.java
+++ Main.java
@@ -1,12 +1,29 @@
-package javaoop.homework1;
-
+package homeWork2;
+import java.util.HashMap;
+import java.util.Map;
 public class Main {
     public static void main(String[] args) {
-        Children child = new Children("Василиса", "Ильина", 9, "Дочь");
-        System.out.println(child);
-        System.out.println(child.mother());
-        System.out.println(child.father());
-        System.out.println(child.grandFather());
-        System.out.println(child.grandMother());
+        Map<String, String> map = new HashMap<>();
+        map.put("Хороший ключ", "Переменная");
+
+        // Инициализация нашего "иммутабельного" класса
+        AlmostMutableClass almostMutable = new AlmostMutableClass("Это неизменяемый", map);
+        MutableClass mutableClass = new MutableClass("Это изменяемый класс", map);
+
+        // Мы не можем изменять состояние объекта через добавление элементов в полученную map
+        System.out.println("Результат в неизменяемом классе после того, как мы получим map");
+        almostMutable.getFieldMap().put("Плохой ключ", "Другое значение");
+        almostMutable.getFieldMap().keySet().forEach(e -> System.out.println(e));
+
+        System.out.println("Результат в изменяемом классе после того, как мы получим map");
+        mutableClass.getFieldMap().put("Плохой ключ", "Другое значение");
+        mutableClass.getFieldMap().keySet().forEach(e -> System.out.println(e));
+
+        System.out.println("");
+        System.out.println("Результат после изменения исходного map в неизменяемом классе");
+        map.put("Иной ключ", "Иное значение");
+        almostMutable.getFieldMap().keySet().forEach(e -> System.out.println(e));
+        System.out.println("Результат после изменения исходного map в изменяемом классе");
+        mutableClass.getFieldMap().keySet().forEach(e -> System.out.println(e));
     }
 }
