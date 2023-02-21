1222823478f4c54ed225450d647d3960d703fe2c
Екатерина <ekaterina@MacBook-Pro-Ekaterina.local>
2023-02-21 10:42:37 +0800

    Выполнен финальный проект

--- Service/impls/MathOperations.java
+++
@@ -1,9 +0,0 @@
-package homeWork5.Service.impls;
-import homeWork5.Model.MathResult;
-
-public interface MathOperations {
-    MathResult sum(int a, int b);
-    MathResult difference(int a, int b);
-    MathResult multiplication(int a, int b);
-    MathResult division(int a, int b);
-}
--- Service/impls/View.java
+++
@@ -1,9 +0,0 @@
-package homeWork5.Service.impls;
-
-import homeWork5.Model.MathResult;
-
-public interface View {
-    void showMathResult(MathResult result);
-    Integer enteringNumbers();
-    int showMenu();
-}
